deb278c4268abd05035e10ed2448ee4701148945
wildmove <muhammad.wildan,ft21@mail.umy.ac.id>
2022-11-11 17:38:43 +0700

    buat method addnote

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -18,5 +18,50 @@ namespace singly_linked_list
         {
             START = null;
         }
+        public void addNote()
+        {
+            int nim;
+            string nm;
+            Console.Write("\n Enter the roll number of student : ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n Enter the name of the student : ");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollNumber = nim;
+            newnode.name = nm;
+
+            //if the node to be inserted is the first node
+            if ((START == null) && (nim <= START.rollNumber))
+            {
+                if ((START == null) && (nim == START.rollNumber)) ;
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return;
+                }
+                newnode.next = START;
+                START = newnode;
+                return;
+            }
+
+            Node previous, current;
+            previous = START;
+            current = START;
+            while ((current != null) && (nim >= current.rollNumber))
+            {
+                if (nim == current.rollNumber)
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return;
+                }
+                previous = current;
+                current = current.next;
+            }
+
+            /*once the above for loop is executed, prev and current are positioned in such a
+              way that the position for the new node */
+            newnode = current;
+            previous.next = newnode;
+        }
+
     }
 }
