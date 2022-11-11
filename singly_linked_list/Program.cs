89f252e5584b000eb181aa467369d3b08bdd6e9f
wildmove <muhammad.wildan,ft21@mail.umy.ac.id>
2022-11-11 20:01:52 +0700

    buat method delNode

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -77,5 +77,18 @@ namespace singly_linked_list
             }
         } 
 
+        public bool delNode(int nim)
+        {
+            Node previous, current;
+            previous = current = null;
+            //check if the spesified node is present in the list or not
+            if(Search(nim, ref previous, ref current) == false)
+                return false;
+            previous.next = current.next;
+            if (current == START)
+                START = START.next;
+            return true;
+        }
+
     }
 }
