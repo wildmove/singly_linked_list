aa4e4dd07f69c59fad7ff12033cd17640d8f2666
wildmove <muhammad.wildan,ft21@mail.umy.ac.id>
2022-11-11 20:14:22 +0700

    buat menu dengan menggunakan while yang didalamnya mengguanakan try catch exception

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -121,6 +121,22 @@ namespace singly_linked_list
         static void Main(String[] args)
         {
             List obj = new List();
+            
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("\nMENU");
+                    Console.WriteLine("1. Add a record to the list ");
+                    Console.WriteLine("2. Delete a record from the list");
+                    Console.WriteLine("3. View all the records in the list");
+                    Console.WriteLine("4. Search for a record in the list");
+                    Console.WriteLine("5. EXIT");
+                    Console.Write("\nEnter your choice (1-5)");
+                    char ch = Convert.ToChar(Console.ReadLine());
+
+                }
+            }
         }
     }
 
